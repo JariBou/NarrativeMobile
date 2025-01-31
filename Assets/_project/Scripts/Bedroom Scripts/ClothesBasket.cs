d6304f7aefe5a47a8b9511442f3dbbf723cee025
Volod_IA <volodiabussereau@gmail.com>
2025-01-31 11:31:17 +0100

    [ADD] Clothes Basket behavior

---
+++ Assets/_project/Scripts/Bedroom Scripts/ClothesBasket.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+using UnityEngine.Events;
+
+public class ClothesBasket : MonoBehaviour
+{
+    [SerializeField] private int _clothesCapacity;
+    private DragTargetZone _dragTargetZone;
+
+    [SerializeField] private Sprite _filledBasketSprite;
+
+    [SerializeField] private UnityEvent _onClothesBasketFilled;
+    private void Awake()
+    {
+        _dragTargetZone = GetComponent<DragTargetZone>();
+        _dragTargetZone.onItemDragged += OnItemDragged;
+    }
+
+    private void OnItemDragged(int nbDraggedItem)
+    {
+        if (nbDraggedItem == _clothesCapacity)
+        {
+            GetComponent<SpriteRenderer>().sprite = _filledBasketSprite;
+            _onClothesBasketFilled?.Invoke();
+        }
+    }
+}
