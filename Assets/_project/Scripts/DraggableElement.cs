950a77c68b5926efab936b9226d32f4a1c0bccc4
Volod_IA <volodiabussereau@gmail.com>
2025-02-01 15:15:23 +0100

    [ADD] Soap with key when cleaned

--- Assets/_project/Scripts/DraggableElement.cs
+++ Assets/_project/Scripts/DraggableElement.cs
@@ -1,6 +1,7 @@
 using System;
 using GraphicsLabor.Scripts.Attributes.LaborerAttributes.InspectedAttributes;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class DraggableElement : MonoBehaviour
 {
@@ -10,11 +11,15 @@ public class DraggableElement : MonoBehaviour
     [SerializeField, ShowIf("_hasTargetZone")] private DragTargetZone _targetZone;
     private Vector3 _defaultPosition;
     private IDraggableItemConstraint[] _constraintInterface;
+
+    [SerializeField] private UnityEvent _onDroppedInTargetZone;
     
     public bool CanBeDragged() => _canBeDragged;
     
     public void AllowDragging() => _canBeDragged = true;
     public void LockDragging() => _canBeDragged = false;
+    
+    public void DestroySelf() => Destroy(gameObject);
 
     private void Awake()
     {
@@ -29,6 +34,7 @@ public class DraggableElement : MonoBehaviour
             if (_targetZone.GetCollider().OverlapPoint(transform.position) && CheckConstraints())
             {
                 _targetZone.DraggedItem(this);
+                _onDroppedInTargetZone?.Invoke();
             }
             else if (_goBackToDefaultPos) transform.position = _defaultPosition;
         }
