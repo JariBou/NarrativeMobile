ec0a03cb42586565fa0045e66b86e7fec1c86f1d
Jari Bou <bourdietome@gmail.com>
2025-02-04 14:22:13 +0100

    euuuuuuh

---
+++ Assets/_project/Scripts/SettingsScript.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.Serialization;
+using UnityEngine.UI;
+
+namespace _project.Scripts
+{
+    public class SettingsScript : MonoBehaviour
+    {
+        [SerializeField] private Slider _masterSlider;
+        [SerializeField] private Slider _sfxSlider;
+        [SerializeField] private Slider _voicesSlider;
+        
+        [SerializeField]
+        private AudioMixer Mixer;
+
+        private void Awake()
+        {
+            _masterSlider.value = PlayerPrefs.GetFloat("volume_master");
+            float val = -80 + _masterSlider.value * 80;
+            Mixer.SetFloat("volume_master", val);
+            
+            _sfxSlider.value = PlayerPrefs.GetFloat("volume_sfx");
+            val = -80 + _sfxSlider.value * 80;
+            Mixer.SetFloat("volume_sfx", val);
+            
+            _voicesSlider.value = PlayerPrefs.GetFloat("volume_voices");
+            val = -80 + _voicesSlider.value * 80;
+            Mixer.SetFloat("volume_voices", val);
+            
+            _masterSlider.onValueChanged.AddListener(MasterSliderValueChanged);
+            _sfxSlider.onValueChanged.AddListener(SfxSliderValueChanged);
+            _voicesSlider.onValueChanged.AddListener(VoicesSliderValueChanged);
+        }
+
+       
+        private void MasterSliderValueChanged(float value)
+        {
+            float val = -80 + value * 80;
+            PlayerPrefs.SetFloat("volume_master", value);
+            Mixer.SetFloat("volume_master", val);
+        }
+
+        private void SfxSliderValueChanged(float value)
+        {
+            float var = -80 + value * 80;
+            PlayerPrefs.SetFloat("volume_sfx", value);
+            Mixer.SetFloat("volume_sfx", var);
+        }
+        
+        private void VoicesSliderValueChanged(float value)
+        {
+            float var = -80 + value * 80;
+            PlayerPrefs.SetFloat("volume_voices", value);
+            Mixer.SetFloat("volume_voices", var);
+        }
+        
+    }
+}
