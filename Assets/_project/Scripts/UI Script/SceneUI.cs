56915f742efea206021128dacd8c00754726a082
Volod_IA <volodiabussereau@gmail.com>
2025-02-04 23:18:15 +0100

    [ADD] Dialogs implemented in Scene

--- Assets/_project/Scripts/UI Script/SceneUI.cs
+++ Assets/_project/Scripts/UI Script/SceneUI.cs
@@ -2,54 +2,57 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class SceneUI : MonoBehaviour
+namespace _project.Scripts.UI_Script
 {
-    [SerializeField] private GameObject _sceneCircle;
-    [SerializeField] private Sprite[] _lockAmountSprites;
+    public class SceneUI : MonoBehaviour
+    {
+        [SerializeField] private GameObject _sceneCircle;
+        [SerializeField] private Sprite[] _lockAmountSprites;
 
-    [SerializeField] private GameObject _goBackButton;
-    [SerializeField] private GameObject _phoneButton;
+        [SerializeField] private GameObject _goBackButton;
+        [SerializeField] private GameObject _phoneButton;
     
-    [Header("Scene Circle Sizes")]
-    [SerializeField] private Vector2 _activeSceneSize = new Vector2(20,20);
-    [SerializeField] private Vector2 _normalSceneSize = new Vector2(10,10);
+        [Header("Scene Circle Sizes")]
+        [SerializeField] private Vector2 _activeSceneSize = new Vector2(20,20);
+        [SerializeField] private Vector2 _normalSceneSize = new Vector2(10,10);
 
-    private GameObject[] _sceneCircles;
+        private GameObject[] _sceneCircles;
 
-    private void Start()
-    {
-        SceneManager.Instance.SetupSceneUI(this);
-        _goBackButton.SetActive(false);
-        _phoneButton.SetActive(true);
-    }
-
-    public void SetupUI(List<RoomScene> roomScenes)
-    {
-        _sceneCircles = new GameObject[roomScenes.Count];
-        for (int i = 0; i < roomScenes.Count; i++)
+        private void Start()
         {
-            _sceneCircles[i] = Instantiate(_sceneCircle, transform);
-            _sceneCircles[i].GetComponent<Image>().sprite = _lockAmountSprites[roomScenes[i].LockAmount];
+            SceneManager.Instance.SetupSceneUI(this);
+            _goBackButton.SetActive(false);
+            _phoneButton.SetActive(true);
         }
-    }
 
-    public void UpdateCurrentRoomUI(int currentRoomIndex)
-    {
-        for (int i = 0; i < _sceneCircles.Length; i++)
+        public void SetupUI(List<RoomScene> roomScenes)
         {
-            _sceneCircles[i].GetComponent<RectTransform>().sizeDelta = i==currentRoomIndex? _activeSceneSize : _normalSceneSize;
-            _sceneCircles[i].GetComponent<Image>().color = new Color(1f, 1f, 1f, currentRoomIndex == i ? 1f : 0.44f);
+            _sceneCircles = new GameObject[roomScenes.Count];
+            for (int i = 0; i < roomScenes.Count; i++)
+            {
+                _sceneCircles[i] = Instantiate(_sceneCircle, transform);
+                _sceneCircles[i].GetComponent<Image>().sprite = _lockAmountSprites[roomScenes[i].LockAmount];
+            }
         }
-    }
 
-    public void ChangeLockAmountOfRoom(int lockAmount, int roomIndex)
-    {
-        _sceneCircles[roomIndex].GetComponent<Image>().sprite = _lockAmountSprites[lockAmount];
-    }
+        public void UpdateCurrentRoomUI(int currentRoomIndex)
+        {
+            for (int i = 0; i < _sceneCircles.Length; i++)
+            {
+                _sceneCircles[i].GetComponent<RectTransform>().sizeDelta = i==currentRoomIndex? _activeSceneSize : _normalSceneSize;
+                _sceneCircles[i].GetComponent<Image>().color = new Color(1f, 1f, 1f, currentRoomIndex == i ? 1f : 0.44f);
+            }
+        }
 
-    public void UpdateChangedSceneElement(SceneElement sceneElement)
-    {
-        _goBackButton.SetActive(!sceneElement.IsMainRoom());
-        _phoneButton.SetActive(sceneElement.IsMainRoom());
+        public void ChangeLockAmountOfRoom(int lockAmount, int roomIndex)
+        {
+            _sceneCircles[roomIndex].GetComponent<Image>().sprite = _lockAmountSprites[lockAmount];
+        }
+
+        public void UpdateChangedSceneElement(SceneElement sceneElement)
+        {
+            _goBackButton.SetActive(!sceneElement.IsMainRoom());
+            _phoneButton.SetActive(sceneElement.IsMainRoom());
+        }
     }
 }
