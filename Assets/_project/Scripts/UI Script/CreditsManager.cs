412f74b47453afcc544b3e0c3e1dc31d8d0cf0ee
Volod_IA <volodiabussereau@gmail.com>
2025-02-06 16:20:00 +0100

    [ADD] Credits Scene

---
+++ Assets/_project/Scripts/UI Script/CreditsManager.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using GraphicsLabor.Scripts.Attributes.LaborerAttributes.DrawerAttributes;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CreditsManager : MonoBehaviour
+{
+    [SerializeField] private GameObject _creditsPanel;
+    [SerializeField] private Vector3 _creditsFinishPosition;
+    [SerializeField] private float _creditsTime;
+    [SerializeField, Scene] private string _mainMenuScene;
+
+    private void Start()
+    {
+        StartCoroutine(CreditsCoroutine());
+    }
+
+    private IEnumerator CreditsCoroutine()
+    {
+        Vector3 basePosition = _creditsPanel.transform.localPosition;
+        for (int i = 0; i < _creditsTime * 100; i++)
+        {
+            float alpha = i / (_creditsTime * 100f - 1);
+            _creditsPanel.transform.localPosition = Vector3.Lerp(basePosition, _creditsFinishPosition, alpha);
+            Debug.Log(alpha);
+            yield return new WaitForSeconds(0.01f);
+        }
+        SceneManager.LoadScene(_mainMenuScene);
+    }
+}
